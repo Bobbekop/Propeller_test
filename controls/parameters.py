f57fb3ac81ea17e9fa7bd0521e1fde6ed717293d
Bobbekop <55990164+Bobbekop@users.noreply.github.com>
2023-11-06 09:49:52 +0100

    Update parameters.py

--- controls/parameters.py
+++ controls/parameters.py
@@ -9,7 +9,7 @@ def parameter_controls():
     with tab1:
         num_of_blades=st.number_input("Number of Blades",min_value=1, value = 2, max_value= 12)
         propeller_diameter_inch=st.number_input("Propeller Diameter(Inch)",min_value=3, value=6,max_value=15)
-        Pitch=st.number_input("Pitch(Inch)",min_value=0,value=4,max_value=10)
+        pitch_inch=st.number_input("Pitch(Inch)",min_value=0,value=4,max_value=10)
         chord_scale=st.number_input("Chord Scale",min_value=0.01,value=0.15,max_value=0.25,step=0.01,format="%.2f")
         tip_size=st.number_input("Tip Size",min_value=0.01,value=5.0,max_value=15.0,step=0.1,format="%.1f")
         
