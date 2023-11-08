535af9186171bbc07a17582d9ca8022acf8c3b09
Bobbekop <55990164+Bobbekop@users.noreply.github.com>
2023-11-08 11:11:28 +0100

    Delete controls/sidebar.py

--- controls/sidebar.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def sidebar():
-    with st.sidebar:
-        st.title('Propeller Generator Test')
-        st.markdown("[Get an Airfoil](http://airfoiltools.com/search/index)")
