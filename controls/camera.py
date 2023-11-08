51ce9b14a5ea78cd1547889e9094b82f847b4f03
Bobbekop <55990164+Bobbekop@users.noreply.github.com>
2023-11-08 09:34:24 +0100

    Update camera.py

--- controls/camera.py
+++ controls/camera.py
@@ -1,12 +1,6 @@
 import streamlit as st
 
 def camera_controls():
-    
-    axis1 = 0 
-    axis2 = 0
-    axis3 = -1
-    focus = 50
-    
     """
     col1, col2, col3 = st.columns(3)
     
@@ -20,8 +14,8 @@ def camera_controls():
     focus = st.number_input("Focus",step=1, value=50)
     """
     return {
-        'axis1':axis1, 
-        'axis2':axis2, 
-        'axis3':axis3, 
-        'focus':focus
+        'axis1':axis1 = 0, 
+        'axis2':axis2 = 0, 
+        'axis3':axis3 = -1, 
+        'focus':focus = 50
     }
