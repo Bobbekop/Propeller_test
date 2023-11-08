03d2e0f100eb18c6d8d84464be009da3c115811d
Bobbekop <55990164+Bobbekop@users.noreply.github.com>
2023-11-08 14:13:08 +0100

    Update __init__.py

--- app/controls/__init__.py
+++ app/controls/__init__.py
@@ -3,5 +3,6 @@ from .modelControls import model_controls
 from .parameters import parameter_controls
 from .sidebar import sidebar
 
+from .layersTable import layers_table
 from .parametersLayers import parameter_controls_layers
 from .codeView import code_view
