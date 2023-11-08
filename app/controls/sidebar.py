2c2b5711cede9ac4fb78412d43449c53c56692d3
Bobbekop <55990164+Bobbekop@users.noreply.github.com>
2023-11-08 11:11:44 +0100

    Create sidebar.py

---
+++ app/controls/sidebar.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def sidebar():
+    with st.sidebar:
+        st.title('Propeller Generator Test')
+        st.markdown("[Get an Airfoil](http://airfoiltools.com/search/index)")
