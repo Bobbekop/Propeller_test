0b7cb80539aee4a39fa06bd62f0236b9176034c3
Bobbekop <55990164+Bobbekop@users.noreply.github.com>
2023-11-08 11:29:57 +0100

    Update sidebar.py

--- app/controls/sidebar.py
+++ app/controls/sidebar.py
@@ -3,5 +3,5 @@ import streamlit as st
 def sidebar():
     with st.sidebar:
         st.title('Propeller Generator Test')
-        st.write('This is a test app, testing the functionality of a parametric propeller design for UAV$'$s, using CadQuery.')
+        st.write('This is a test app, testing the functionality of a parametric propeller design for UAVs, using CadQuery.')
         st.markdown("[Get an Airfoil](http://airfoiltools.com/search/index)")
